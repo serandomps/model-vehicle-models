1fbf85e6f3df67846f5a78c36ec32108bd124405
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-05-22 16:52:00 +0530

    updating

---
+++ service/index.js
@@ -0,0 +1,54 @@
+var utils = require('utils');
+var serand = require('serand');
+
+var makes = {};
+var models = {};
+
+var process = function (data, done) {
+    data.forEach(function (model) {
+        models[model.id] = model;
+        var o = makes[model.make] || (makes[model.make] = []);
+        o.push(model);
+    });
+    done();
+};
+
+var find = function (done) {
+    utils.sync('vehicle-models:find', function (ran) {
+        $.ajax({
+            method: 'GET',
+            url: utils.resolve('autos:///apis/v/vehicle-models'),
+            dataType: 'json',
+            success: function (data) {
+                process(data, ran);
+            },
+            error: function (xhr, status, err) {
+                ran(err || status || xhr);
+            }
+        });
+    }, done);
+};
+
+exports.findOne = function (id, done) {
+    if (models[id]) {
+        return done(null, models[id]);
+    }
+    find(function (err) {
+        if (err) {
+            return done(err);
+        }
+        done(null, models[id]);
+    });
+};
+
+exports.find = function (make, done) {
+    if (makes[make]) {
+        return done(null, makes[make]);
+    }
+    find(function (err) {
+        if (err) {
+            return done(err);
+        }
+        done(null, makes[make]);
+    });
+};
