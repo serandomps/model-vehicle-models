6e5b591ab375e1a3b4f777f3e121f7dfbfa66dbb
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-10-18 08:43:38 +0530

    updating

--- service/index.js
+++ service/index.js
@@ -13,18 +13,19 @@ var process = function (data, done) {
     done();
 };
 
-var find = function (done) {
+var find = function (make, done) {
     utils.sync('vehicle-models:find', function (ran) {
-        $.ajax({
-            method: 'GET',
-            url: utils.resolve('autos:///apis/v/vehicle-models'),
-            dataType: 'json',
-            success: function (data) {
-                process(data, ran);
+        var next = utils.resolve('autos:///apis/v/vehicle-models' + utils.data({
+            query: {
+                make: make
             },
-            error: function (xhr, status, err) {
-                ran(err || status || xhr);
+            count: 100
+        }));
+        utils.all(next, function (err, o) {
+            if (err) {
+                return ran(err);
             }
+            ran(null, _.sortBy(o, 'title'));
         });
     }, done);
 };
@@ -45,10 +46,11 @@ exports.find = function (make, done) {
     if (makes[make]) {
         return done(null, makes[make]);
     }
-    find(function (err) {
+    find(make, function (err, o) {
         if (err) {
             return done(err);
         }
-        done(null, makes[make]);
+        makes[make] = o;
+        done(null, o);
     });
 };
